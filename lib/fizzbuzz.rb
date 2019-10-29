f8d0590288cacee9585da8338fee71b6ceaa512f
VINCENT SAVAGE <savagevince@hotmail.com>
2019-10-29 15:14:44 +0000

    second commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,14 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    describe 'fizzbuzz' do
+      it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+      end
+end
+end
 end
